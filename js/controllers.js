2fbc3c1a3d7cf106a039503bbffab9e54a25b2fc
sonhoang0611 <hoangthaison985@gmail.com>
2016-05-04 21:44:11 +0700

    Cập nhật lại thư mục js

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* Controllers */
-var phonecatApp = angular.module('myApp', []);
+var phonecatApp = angular.module('myApp', ['shaka-editme']);
 
 phonecatApp.controller('mywebCtrl', function ($scope, $http) {
   $http.get('profile.json').success(function(data) {
